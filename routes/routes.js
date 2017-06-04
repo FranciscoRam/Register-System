cbcade11f56db2e7e6bb6470e5a57d89dddb8bd0
FranciscoRam <r.g.francisco40@gmail.com>
2017-06-04 12:50:59 -0500

    renombrando, eliminando y add folder controllers

---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var controllers = require('.././controllers');
+/* GET home page.
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+*/
+
+router.get('/', controllers.HomeController.index);
+
+module.exports = router;
